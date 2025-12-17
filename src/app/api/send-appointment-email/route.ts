fe7458bfc5cf67be4242de649ac8297e35e928a7
Yash20M <yash65mahalle@gmail.com>
2025-12-17 16:48:07 +0530

    email send

---
+++ src/app/api/send-appointment-email/route.ts
@@ -0,0 +1,42 @@
+import AppointmentConfirmationEmail from "@/components/emails/AppointmentConfirmationEmail";
+import resend from "@/lib/resend";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+
+        const { userEmail, doctorName, appointmentDate, appointmentTime, appointmentType, duration, price } = body
+
+        if (!userEmail || !doctorName || !appointmentDate || !appointmentTime) {
+            return NextResponse.json({ error: "Missing required field" }, { status: 400 })
+        }
+
+        const { data, error } = await resend.emails.send({
+            from: "Dentiwise <yash@resend.dev>",
+            to: [userEmail],
+            subject: "Appointment Confirmation - Dentwise",
+            react: AppointmentConfirmationEmail({
+                doctorName,
+                appointmentDate,
+                appointmentTime,
+                appointmentType,
+                duration,
+                price
+            })
+        })
+
+        if (error) {
+            return NextResponse.json({ error: "Failed to send email" }, { status: 500 })
+        }
+
+        return NextResponse.json({
+            message: "Appointment confirmation email sent successfully",
+            emailId: data?.id
+        }, { status: 200 })
+    }
+    catch (err) {
+        console.log(err)
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 }) 
+    }
+}
